e4e024c93c3f72b1ee7a4a4da11a2ca9dc66f5e0
Thiago Pereira <thiagop@empregoligado.com.br>
2015-08-17 16:33:36 -0300

    Fluxo parcial de compra.

--- cucumber/features/step_definitions/my_steps.rb
+++ cucumber/features/step_definitions/my_steps.rb
@@ -1,25 +1,25 @@
 # encoding: iso-8859-1
 #!/usr/bin/env ruby
 
-Dado(/^que eu acesso o walmart$/) do
+Dado(/^que eu acessei o walmart$/) do
   visit 'http://www.walmart.com.br'
 end
 
 Quando(/^eu buscar por um produto do tipo televisao$/) do
-  page.find(:id, 'suggestion-search').click
-  fill_in'suggestion-search',with=> 'TV'
-  click_button 'search-icon'
-  page.all(:id, 'category-products')[2].click
+  fill_in 'suggestion-search', with: "TV"
+  click_button 'Procurar'
+  page.all(:css, '.product-title')[0].click
+  sleep 03
   end
 
 E(/^adicionar no carrinho$/) do
-  have_content'Smart TV LED 3D Ultra HD/4k Curva 55 Samsung UN55JU7500 Quad Core Clear Motion Rate 1200Hz Função Game'
-  click_button'buy-button btn btn-success'
-  click_button'mfp-close'
+  click_button 'Adicionar ao carrinho'
+  click_button 'Continuar'
   end
 
 Entao(/^o item deve estar no carrinho$/) do
-  click_button'cart-icon'
-  have_content 'Meu Carrinho'
-  have_content '1item'
-  have_content'Smart TV LED 3D Ultra HD/4k Curva 55'
+  find(:css, '.topbar-buttons.open-link.cart-link').click
+  expect(page).to have_content 'Smart TV LED Full HD'
+  find(:css, '#btn-finalize-cart').click
+  find(:css, '.mfp-close').click
+end
