e373b5dab951cccbf781d85107bdc324407051a8
Thiago Pereira <thiagop@empregoligado.com.br>
2015-08-17 14:37:08 -0300

    Fluxo de compra WM

--- cucumber/features/specifications/testeecommerce.feature
+++ cucumber/features/specifications/testeecommerce.feature
@@ -1,21 +1,12 @@
 # language: pt
 
-Funcionalidade: Site Ecommerce Walmart
+Funcionalidade: Compra de televisor
+
+Cenario: Compra de televisor
+
+  Dado que eu acessei o walmart
+  Quando eu buscar por um produto do tipo televisao
+  E adicionar no carrinho
+  Entao o item deve estar no carrinho
+
   
-  Cenario: Entrar no Site e relizar busca por Produto TV
-    
-    Dado Eu entro no site do Walmart
-    Quando  Entao eu digito TV na barra de busca
-    E   Clico no botao buscar
-    Entao  Eu recebo o resultado da busca por produto
-
-  Cenario: Validar resultados da busca e Visualizar um Produto TV selecionado
-    
-    Dado Eu verifico se a busca me retornou algum resultado valido nao pode estar vazia
-    Quando  Então Eu clico em um modelo de TV retornado pela busca
-    Entao  Eu visualizo a pagina com a informacoes do produto que cliquei
-
-  Cenario: Adicionar um Produto e Validar Produto no Carrinho
-    Dado Eu clico no produto para adicionar ao meu carrinho de compras
-    Quando  Eu clico no icone carrinho de compras para efetivar a transacao
-    Entao  Entao Eu visualizo a pagina do carrinho de compras e vejo que meu produto foi adicionado corretamente
--- cucumber/features/step_definitions/my_steps.rb
+++ cucumber/features/step_definitions/my_steps.rb
@@ -1,37 +1,20 @@
 # encoding: utf-8
 #!/usr/bin/env ruby
 
-Dado(/^Eu entro no site do Walmart$/) do
-  visit "http://www.walmart.com.br/busca"
-end
-
-Quando(/^Entao eu digito "([^"]*)" na barra de "([^"]*)"$/) do |arg1, arg2|
+Dado(/^que eu acessei o walmart$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Quando(/^Clico no botao "([^"]*)"$/) do |arg1|
+Quando(/^eu buscar por um produto do tipo televisao$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Entao(/^Eu recebo o resultado da busca por produto$/) do
+E(/^adicionar no carrinho$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Dado(/^Eu verifico se a busca me retornou algum resultado valido nao pode estar "([^"]*)"$/) do |arg1|
+Entao(/^o item deve estar no carrinho$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
-
-Dado(/^Eu clico no produto para adicionar ao meu "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Quando(/^Eu clico no icone "([^"]*)" para efetivar a transacao$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Entao(/^Entao Eu visualizo a pagina do "([^"]*)" e vejo que meu produto foi adicionado corretamente$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 
 
