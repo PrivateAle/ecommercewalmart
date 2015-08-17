935912c5b0a96b5e34a82c4c5591095c88685e42
Adolce <Adolce@users.noreply.github.com>
2015-08-17 15:13:54 -0300

    Update my_steps.rb
    Atualizando implementação

--- cucumber/features/step_definitions/my_steps.rb
+++ cucumber/features/step_definitions/my_steps.rb
@@ -2,19 +2,24 @@
 #!/usr/bin/env ruby
 
 Dado(/^que eu acessei o walmart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit 'http://www.walmart.com.br'
 end
 
 Quando(/^eu buscar por um produto do tipo televisao$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  page.find(:id, 'suggestion-search').click
+  fill_in'suggestion-search',with=> 'TV'
+  click_button 'search-icon'
+  page.all(:id, 'category-products')[2].click
+  end
 
 E(/^adicionar no carrinho$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  have_content'Smart TV LED 3D Ultra HD/4k Curva 55 Samsung UN55JU7500 Quad Core Clear Motion Rate 1200Hz Função Game'
+  click_button'buy-button btn btn-success'
+  click_button'mfp-close'
+  end
 
 Entao(/^o item deve estar no carrinho$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-
+  click_button'cart-icon'
+  have_content 'Meu Carrinho'
+  have_content '1item'
+  have_content'Smart TV LED 3D Ultra HD/4k Curva 55'
