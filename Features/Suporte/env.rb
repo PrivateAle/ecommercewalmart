123b66cf27d0fa359d8eadbeb6b95aefd29694c4
Thiago Marques Pereira <thiagomarquessp@gmail.com>
2015-08-17 13:17:18 -0300

    Update env.rb

--- Features/Suporte/env.rb
+++ Features/Suporte/env.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'../../','lib'))
 
+require 'capybara/cucumber'
+require "selenium-webdriver"
 require 'rspec-expectations'
 require 'page-object'
 require 'require_all'
