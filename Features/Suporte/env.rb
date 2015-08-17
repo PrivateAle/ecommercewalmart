9faee399916d16d1be5573fe2d04b0225d4b241c
Thiago Marques Pereira <thiagomarquessp@gmail.com>
2015-08-17 13:29:51 -0300

    Update env.rb

--- Features/Suporte/env.rb
+++ Features/Suporte/env.rb
@@ -11,3 +11,10 @@ require 'fig_newton'
 require_all 'lib'
 
 World(PageObject::PageFactory)
+
+  Capybara::Webkit.configure do |config|
+    config.allow_url("https://www2.walmart.com.br/checkout/content/carrinho")
+    config.block_unknown_urls
+    config.allow_url "http://www.walmart.com.br/busca"
+    config.allow_url "http://www.walmart.com.br"
+  end
